2f9bc420817cd10856071195a482ebc96fad5ea8
Asmita <121007767+Asmita056@users.noreply.github.com>
2024-06-27 11:05:35 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import LoginPage from './components/LoginPage'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <LoginPage/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage(){
+    return (
+        <div>
+            <h1 className="bg-red-600 w-1/4">heloooooooooooo</h1>
+        </div>
+    )
+}
